8596f3367169c34013c64a0c7bafd793ad4c3eae
Jordy Frias <j.frias@kation.com.do>
2023-05-31 14:41:38 -0400

    chore: :sparkles: Add setup.js

--- install.js
+++ install.js
@@ -53,5 +53,3 @@ function setupProject() {
 module.exports = {
   setupProject,
 };
-
-setupProject();
---
+++ setup.js
@@ -0,0 +1,4 @@
+const { execSync } = require('child_process');
+const { setupProject } = require('./install.js');
+
+setupProject();
